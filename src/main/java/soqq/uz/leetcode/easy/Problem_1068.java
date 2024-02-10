206c818061313837191e9aed4bfabb0dde93dada
Jahongir <sjahongir22@gmail.com>
2024-02-10 19:32:44 +0500

    Problem_1068

---
+++ src/main/java/soqq/uz/leetcode/easy/Problem_1068.java
@@ -0,0 +1,12 @@
+package soqq.uz.leetcode.easy;
+
+public class Problem_1068 {
+        /*
+      Paths :
+        https://leetcode.com/problems/product-sales-analysis-i
+    */
+
+    // Solution :
+
+    /* select product_name, year , price from Sales Join Product on Sales.product_id = Product.product_id; */
+}
