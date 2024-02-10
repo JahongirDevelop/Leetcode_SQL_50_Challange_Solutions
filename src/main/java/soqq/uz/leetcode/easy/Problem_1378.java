523d0d317b94d6c4c22bdafc59960f6dbc9683c9
Jahongir <sjahongir22@gmail.com>
2024-02-10 19:13:05 +0500

    Problem_1378

---
+++ src/main/java/soqq/uz/leetcode/easy/Problem_1378.java
@@ -0,0 +1,12 @@
+package soqq.uz.leetcode.easy;
+
+public class Problem_1378 {
+    /*
+      Paths :
+        https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier
+    */
+
+    // Solution :
+
+    /* select unique_id, name from Employees left join EmployeeUNI on Employees.id = EmployeeUNI.id; */
+}
