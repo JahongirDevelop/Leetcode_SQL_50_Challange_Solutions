b9a62521a5787909e7fc40591ed7428af03ef486
Jahongir <sjahongir22@gmail.com>
2024-02-10 18:57:10 +0500

    Problem_1148

---
+++ src/main/java/soqq/uz/leetcode/easy/Problem_1148.java
@@ -0,0 +1,12 @@
+package soqq.uz.leetcode.easy;
+
+public class Problem_1148 {
+    /*
+      Paths :
+       https://leetcode.com/problems/article-views-i
+    */
+
+    // Solution :
+
+    /* select distinct author_id as id from Views where author_id = viewer_id order by id  asc;   */
+}
