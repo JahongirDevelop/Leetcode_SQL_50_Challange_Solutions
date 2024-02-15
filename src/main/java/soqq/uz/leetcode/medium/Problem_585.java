234f31b0e92a8fac166a779c9e5833e678b94f3f
Jahongir <sjahongir22@gmail.com>
2024-02-15 22:00:13 +0500

    Problem_585

--- src/main/java/soqq/uz/leetcode/medium/Problem_585.java
+++ src/main/java/soqq/uz/leetcode/medium/Problem_585.java
@@ -8,5 +8,19 @@ public class Problem_585 {
 
     // Solution :
 
-    /*select max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from employee); */
+    /*SELECT CAST(SUM(tiv_2016) AS NUMERIC(10, 2)) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) NOT IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) > 1
+);
+ */
 }
