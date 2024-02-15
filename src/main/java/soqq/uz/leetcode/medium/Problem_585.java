595f838b6e4d93bea09423ee1e42b6b0ebedb3b3
Jahongir <sjahongir22@gmail.com>
2024-02-15 21:59:22 +0500

    Problem_585

---
+++ src/main/java/soqq/uz/leetcode/medium/Problem_585.java
@@ -0,0 +1,12 @@
+package soqq.uz.leetcode.medium;
+
+public class Problem_585 {
+    /*
+      Paths :
+       https://leetcode.com/problems/investments-in-2016
+    */
+
+    // Solution :
+
+    /*select max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from employee); */
+}
